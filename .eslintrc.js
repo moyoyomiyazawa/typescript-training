bd274db6aaec11f55ee789e6f3c73845ef12f4a0
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-05-24 16:32:26 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  root: true,
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Hello from './hello';
+
+const root = document.getElementById('root');
+const hello = new Hello('Hello, TypeScript');
+
+hello.sayHello(root);
